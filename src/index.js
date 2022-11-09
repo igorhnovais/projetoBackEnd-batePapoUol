9318be0db68e5012897d9c7cf21fc58fe7fb8eb4
Igor Novais <igorhnovaisdriven@gmail.com>
2022-11-09 14:48:21 -0300

    logica base feita!

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(express.json()); 
 
 
-
 const mongoClient = new MongoClient("mongodb://localhost:27017"); // conectando a porta do mongo
 let db;
 
@@ -17,12 +16,11 @@ mongoClient.connect().then(() => {
 })
 
 
-
 app.post("/participants", (req, res) => {
 
     const {name} = req.body;
 
-    if(!name || name === "" || name === Number){
+    if(!name || name === Number){
         return res.sendStatus(422);
     }
 
@@ -30,15 +28,55 @@ app.post("/participants", (req, res) => {
 
     db.collection("user").insertOne(user)
     .then(res.sendStatus(201))
-    .catch(res.sendStatus(500)) // inserindo o nome no mongo
+    .catch(res.sendStatus(500)); // inserindo o nome no mongo
 });
 
 app.get("/participants", (req, res) => {
-    db.collection("user").find().toArray().then(user => {
+    db.collection("user").find().toArray()
+    .then(user => {
         res.send(user)
-    }).catch(res.send("erro no banco de dados"));
+    })
+    .catch(res.send("erro no banco de dados"));
 });
 
+app.post("/messages", (req,res) => {
+
+    const {to, text, type} = req.body;
+    const {from} = req.header;
+
+    if(!to || !text || !type){
+        return res.sendStatus(422);
+    }
+
+    const messageCreated = {
+        to,
+        text,
+        type
+    }
+
+    db.collection("messages").insertOne(messageCreated)
+    .then(res.sendStatus(201))
+    .catch(res.sendStatus(500));
+});
+
+app.get("/messages", (req, res) => {
+    db.collection("messages").find().toArray()
+    .then(message => {
+        console.log(message)
+    })
+    .catch(res.send("erro no banco de dados"))
+});
+
+app.post("/status", (req, res) => {
+    const {User} = req.header
+
+    if(!User){
+        return res.sendStatus(404)
+    }
+
+    res.sendStatus(200);
+
+});
 
 
 app.listen (5000, () => {
